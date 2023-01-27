34f908b7403c8d617697e6e2663ca6998fbfbf46
Uiratan Cavalcante <uiratan@gmail.com>
2023-01-27 10:36:31 -0300

    inicio aula 3

--- index.js
+++ index.js
@@ -3,61 +3,91 @@ import chalk from 'chalk';
 
 function trataErro(erro) {
   console.log(erro);
-  throw new Error(chalk.red(erro.code, 'arquivo inexistente'));
+  //throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-// função assíncrona com async/await
-// async é para informar que DENTRO da função vai existir código assíncrono
-// permite usar try/catch
+// async/await
+
 async function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
   try {
+    const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
-  } catch(erro) {
-    trataErro(erro);
-  } finally {
-    console.log(chalk.yellow('operação concluída'));
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro)
   }
- }
-
-await pegaArquivo('./arquivos/texto.md');
-
-pegaArquivo('./arquivos/texto.mdx');
+}
 
 
-// função assíncrona com then
-// function pegaArquivo(caminhoDoArquivo) { 
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = 'utf-8';
 //   fs.promises
 //     .readFile(caminhoDoArquivo, encoding)
 //     .then((texto) => console.log(chalk.green(texto)))
-//     // .catch((erro) => trataErro(erro));
-//     // como catch é uma função que recebe outra função como 
-//     // parametro, basta passar como abaixo
-//     .catch(trataErro);
+//     .catch(trataErro)
 // }
 
-// funcção síncrona
-// function pegaArquivo(caminhoDoArquivo) {
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
+
+
+// import fs from 'fs';
+// import chalk from 'chalk';
+
+// function trataErro(erro) {
+//   console.log(erro);
+//   throw new Error(chalk.red(erro.code, 'arquivo inexistente'));
+// }
+
+// // função assíncrona com async/await
+// // async é para informar que DENTRO da função vai existir código assíncrono
+// // permite usar try/catch
+// async function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = 'utf-8';
-//   // caminho, encoding, função de callback realizada com o resultado do
-//   // readFile. essa função recebe 2 parametros: erro caso aconteça e 
-//   // o proprio contudo do arquivo
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       trataErro(erro);
-//       //console.log(erro);
-//     }
+//   try {
+//     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+//     console.log(chalk.green(texto))
+//   } catch(erro) {
+//     trataErro(erro);
+//   } finally {
+//     console.log(chalk.yellow('operação concluída'));
+//   }
+//  }
 
-//     console.log(chalk.green(texto));
-//   });
-// }
+// pegaArquivo('./arquivos/texto.md');
 
-
-
+// pegaArquivo('./arquivos/texto.mdx');
 
 
+// // função assíncrona com then
+// // function pegaArquivo(caminhoDoArquivo) { 
+// //   const encoding = 'utf-8';
+// //   fs.promises
+// //     .readFile(caminhoDoArquivo, encoding)
+// //     .then((texto) => console.log(chalk.green(texto)))
+// //     // .catch((erro) => trataErro(erro));
+// //     // como catch é uma função que recebe outra função como 
+// //     // parametro, basta passar como abaixo
+// //     .catch(trataErro);
+// // }
+
+// // funcção síncrona
+// // function pegaArquivo(caminhoDoArquivo) {
+// //   const encoding = 'utf-8';
+// //   // caminho, encoding, função de callback realizada com o resultado do
+// //   // readFile. essa função recebe 2 parametros: erro caso aconteça e 
+// //   // o proprio contudo do arquivo
+// //   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+// //     if (erro) {
+// //       trataErro(erro);
+// //       //console.log(erro);
+// //     }
+
+// //     console.log(chalk.green(texto));
+// //   });
+// // }
 
 
 
@@ -69,22 +99,27 @@ pegaArquivo('./arquivos/texto.mdx');
 
 
 
-// console.log(chalk.blue('olá mundo!'));
 
-// console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
-// console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
 
-// console.log(chalk.blue.bgWhite.bold('Alura'));
 
-// //encadear métodos para colorir texto, cor de fundo e texto em negrito
-// console.log(chalk.blue.bgWhite.bold('Alura'));
-// //receber múltiplos argumentos
-// console.log(chalk.blue('curso', 'de', 'Node.js'));
-// //métodos aninhados
-// console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// // uso de template strings e placeholders
-// console.log(`
-// CPU: ${chalk.red('90%')}
-// RAM: ${chalk.green('40%')}
-// DISK: ${chalk.yellow('70%')}
-// `);
+
+
+// // console.log(chalk.blue('olá mundo!'));
+
+// // console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+// // console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+
+// // console.log(chalk.blue.bgWhite.bold('Alura'));
+
+// // //encadear métodos para colorir texto, cor de fundo e texto em negrito
+// // console.log(chalk.blue.bgWhite.bold('Alura'));
+// // //receber múltiplos argumentos
+// // console.log(chalk.blue('curso', 'de', 'Node.js'));
+// // //métodos aninhados
+// // console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+// // // uso de template strings e placeholders
+// // console.log(`
+// // CPU: ${chalk.red('90%')}
+// // RAM: ${chalk.green('40%')}
+// // DISK: ${chalk.yellow('70%')}
+// // `);
